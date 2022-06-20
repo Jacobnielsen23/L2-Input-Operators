e5f217c0e09c6ebf1867cc706f872a3e4c49d3ee
jacobnielsen23 <107879349+Jacobnielsen23@users.noreply.github.com>
2022-06-20 20:13:46 +0000

    Madlib

---
+++ main.py
@@ -0,0 +1,24 @@
+'''userName = input("Enter your name: ")
+print("Hey " + userName)
+
+userGrade = input("What grade are you in?: ")
+print("Hey " + userName + ", " + userGrade + " is a lame grade")
+
+userYear = int(input("What year were you born? "))
+userAge = 2022 - userYear
+print(str(userAge) + " is so young!")'''
+
+# Create your own Madlibs 
+# Have at least 5 inputs
+
+name = input("Please give me a name: ")
+verb = input("Please give me a verb: ")
+location = input("Please give me a location: ")
+name2 = input("Please give me a name : ")
+adverb = input("Please give me a adverb: ")
+noun = input("Please give me a noun: ")
+song = input("Please give me a song: ")
+adverb2 = input("Please give me a adverb: ")
+proper_noun= input("Please give me a proper noun: ")
+
+print("I knew a person named " + name + " his favorite activity was to " + verb + " and he usually hung out at " + location + " His best friend's name was" + name2 + "we always did karoke together" + adverb + " next to a" + noun + " our favorite song to sing was" + song + " Our duets are usually" + adverb2 + " however we do have one fan and his name is" + proper_noun )
